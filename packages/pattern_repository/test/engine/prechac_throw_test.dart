dd5fada3ba63e55a60d083c29ef46912773a21ce
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2021-04-26 13:15:46 +0200

    add some tests for PrechacThrow extension

--- packages/pattern_repository/test/engine/prechac_throw_test.dart
+++ packages/pattern_repository/test/engine/prechac_throw_test.dart
@@ -1,1 +1,43 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fraction/fraction.dart';
+
+import 'package:pattern_repository/src/models/throw.dart';
+import 'package:pattern_repository/src/engine/prechac_throw.dart';
+
+void main() {
+  group('PrechacThrow', () {
+    test('calculates landing site for self', () {
+      expect(
+        Throw.self(height: 4).landingSite(
+          position: 0,
+          period: 4,
+          prechator: 2.toFraction(),
+        ),
+        0,
+      );
+    });
+
+    test('calculates landing site for pass', () {
+      expect(
+        Throw(height: Fraction(5, 3), passingIndex: 2).landingSite(
+          position: 4,
+          period: 4,
+          prechator: Fraction(4, 3),
+        ),
+        3,
+      );
+    });
+
+    test('throws an error when calculating landing site with invalid throw',
+        () {
+      expect(
+        () => Throw(height: Fraction(5, 3), passingIndex: 1).landingSite(
+          position: 4,
+          period: 4,
+          prechator: Fraction(4, 3),
+        ),
+        throwsException,
+      );
+    });
+  });
+}
