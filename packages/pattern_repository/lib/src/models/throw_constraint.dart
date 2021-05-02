0ba055619a9a7e1a1544bbfe92bd0871c086dfe3
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2021-05-02 16:38:07 +0200

    unify duplicate code from Throw and ThrowConstraint into Throwable

--- packages/pattern_repository/lib/src/models/throw_constraint.dart
+++ packages/pattern_repository/lib/src/models/throw_constraint.dart
@@ -1,10 +1,8 @@
-import 'package:equatable/equatable.dart';
 import 'package:fraction/fraction.dart';
-import 'package:sprintf/sprintf.dart';
 
 import 'throwable.dart';
 
-class ThrowConstraint extends Throwable with EquatableMixin {
+class ThrowConstraint extends Throwable {
   const ThrowConstraint({
     required this.height,
     required this.passingIndex,
@@ -33,50 +31,4 @@ class ThrowConstraint extends Throwable with EquatableMixin {
   final Fraction? height;
   @override
   final int? passingIndex;
-
-  @override
-  List<Object?> get props => [height, passingIndex];
-
-  bool get isSelf {
-    return passingIndex != null && passingIndex == 0;
-  }
-
-  bool get isPass {
-    return passingIndex != null && passingIndex != 0;
-  }
-
-  bool get isPlaceholder {
-    return passingIndex == null && height == null;
-  }
-
-  @override
-  String toString() {
-    if (isPlaceholder) {
-      return '_';
-    }
-
-    if (isSelf) {
-      return heightToString();
-    }
-
-    return '${heightToString()}p${passingIndexToString()}';
-  }
-
-  String heightToString() {
-    final height = this.height;
-    if (height == null) {
-      return '_';
-    }
-
-    return sprintf('%.4g', [height.toDouble()]);
-  }
-
-  String passingIndexToString() {
-    final passingIndex = this.passingIndex;
-    if (passingIndex == null) {
-      return '_';
-    }
-
-    return passingIndex.toString();
-  }
 }
--- packages/pattern_repository/lib/src/models/throwable.dart
+++ packages/pattern_repository/lib/src/models/throwable.dart
@@ -1,8 +1,108 @@
 import 'package:fraction/fraction.dart';
 
-abstract class Throwable {
+import '../core/core.dart';
+
+abstract class Throwable with Comparable<Throwable>, Compare<Throwable> {
   const Throwable();
 
   Fraction? get height;
   int? get passingIndex;
+
+  @override
+  int compareTo(Throwable other) {
+    // TODO: refactor
+    final thisHeight = height;
+    final otherHeight = other.height;
+
+    if (thisHeight == null && otherHeight == null) {
+      return 0;
+    }
+
+    if (thisHeight == null) {
+      return -1;
+    }
+
+    if (otherHeight == null) {
+      return 1;
+    }
+
+    final heightComparator = thisHeight.compareTo(otherHeight);
+    if (heightComparator != 0) {
+      return heightComparator;
+    }
+
+    final thisPassingIndex = passingIndex;
+    final otherPassingIndex = other.passingIndex;
+
+    if (thisPassingIndex == null && otherPassingIndex == null) {
+      return 0;
+    }
+
+    if (thisPassingIndex == null) {
+      return -1;
+    }
+
+    if (otherPassingIndex == null) {
+      return 1;
+    }
+
+    final passingIndexComparator =
+        thisPassingIndex.compareTo(otherPassingIndex);
+    return passingIndexComparator;
+  }
+
+  bool get isSelf {
+    return passingIndex != null && passingIndex == 0;
+  }
+
+  bool get isPass {
+    return passingIndex != null && passingIndex != 0;
+  }
+
+  bool get isPlaceholder {
+    return passingIndex == null && height == null;
+  }
+
+  @override
+  String toString() {
+    if (isPlaceholder) {
+      return '_';
+    }
+
+    if (isSelf) {
+      return heightToString();
+    }
+
+    return '${heightToString()}p${passingIndexToString()}';
+  }
+
+  String heightToString() {
+    final height = this.height;
+    if (height == null) {
+      return '_';
+    }
+
+    if (height.isWhole) {
+      return height.toString();
+    }
+
+    final decimalPart = height.toDouble() % 1;
+    if (decimalPart.toString().length < 5) {
+      return height.toDouble().toString();
+    }
+
+    final mod = 10.0;
+    final truncatedToOneDecimalDigit =
+        (height.toDouble() * mod).truncateToDouble() / mod;
+    return truncatedToOneDecimalDigit.toString();
+  }
+
+  String passingIndexToString() {
+    final passingIndex = this.passingIndex;
+    if (passingIndex == null) {
+      return '_';
+    }
+
+    return passingIndex.toString();
+  }
 }
