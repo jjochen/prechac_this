b6bff9b28f196b545fc0a9c01b47846deeca637e
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2021-05-05 22:20:16 +0200

    add missing test

--- test/attributions/view/attributions_page_test.dart
+++ test/attributions/view/attributions_page_test.dart
@@ -5,6 +5,9 @@ import 'package:prechac_this/attributions/attributions.dart';
 
 void main() {
   group('AttributionsPage', () {
+    test('has a route', () {
+      expect(AttributionsPage.route(), isA<MaterialPageRoute>());
+    });
     testWidgets('renders icon', (tester) async {
       await tester.pumpWidget(MaterialApp(home: AttributionsPage()));
       expect(find.byKey(const Key('application_icon_image')), findsOneWidget);
