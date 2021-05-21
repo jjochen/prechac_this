2bf1f7907d7863b88fe3fb588aab4ae6129bccbc
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2021-05-21 13:44:30 +0200

    Add more tests for cartesian product iterable

--- test/repositories/pattern_repository/core/cartesian_product_iterable_test.dart
+++ test/repositories/pattern_repository/core/cartesian_product_iterable_test.dart
@@ -3,26 +3,42 @@ import 'package:prechac_this/repositories/pattern_repository/core/cartesian_prod
 
 void main() {
   group('CartesianProductIterable', () {
-    late CartesianProductIterable iterable;
-    setUp(() {
-      iterable = CartesianProductIterable([
-        [1, 2, 3],
+    test('calculates cartesian product', () {
+      final iterable = CartesianProductIterable([
+        [1, null, 3],
         [4],
         [5, 6],
       ]);
-    });
-
-    test('calculates cartesian product', () {
       expect(
         iterable.toList(),
         [
           [1, 4, 5],
           [1, 4, 6],
-          [2, 4, 5],
-          [2, 4, 6],
+          [null, 4, 5],
+          [null, 4, 6],
           [3, 4, 5],
           [3, 4, 6],
         ],
+      );
+    });
+
+    test('does not fail with empty list', () {
+      final iterable = CartesianProductIterable([
+        [1, 2, 3],
+        [],
+        [5, 6],
+      ]);
+      expect(
+        iterable.toList(),
+        [],
+      );
+    });
+
+    test('does not fail with no lists', () {
+      final iterable = CartesianProductIterable([]);
+      expect(
+        iterable.toList(),
+        [],
       );
     });
   });
