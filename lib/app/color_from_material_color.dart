87fa08a9a2ed87b7db9741725bfccf32260f1f38
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2021-07-14 14:07:12 +0200

    use material color swatch to define primary colors

---
+++ lib/app/color_from_material_color.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+extension ColorToMaterialColor on Color {
+  MaterialColor toMaterialColor() {
+    final swatch = <int, Color>{};
+
+    const strengths = <double>[
+      .05,
+      0.1,
+      0.2,
+      0.3,
+      0.4,
+      0.5,
+      0.6,
+      0.7,
+      0.8,
+      0.9
+    ];
+
+    for (final strength in strengths) {
+      final ds = 0.5 - strength;
+      final shade = _shadeForDistance(ds);
+      swatch[(strength * 1000).round()] = shade;
+    }
+    return MaterialColor(value, swatch);
+  }
+
+  Color _shadeForDistance(double ds) {
+    final shade = Color.fromRGBO(
+      _moveChannel(red, ds),
+      _moveChannel(green, ds),
+      _moveChannel(blue, ds),
+      1,
+    );
+    return shade;
+  }
+
+  int _moveChannel(int channel, double ds) {
+    return channel + ((ds < 0 ? channel : (255 - channel)) * ds).round();
+  }
+}
--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -2,21 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'color_from_material_color.dart';
+
 final lightTheme = ThemeData(
   brightness: Brightness.light,
   textTheme: GoogleFonts.openSansTextTheme(
     ThemeData.light().textTheme,
   ),
-  colorScheme: const ColorScheme.light(
-    primary: Color(0xff457b9d),
-    primaryVariant: Color(0xff1d3557),
-    onPrimary: Colors.white,
-    secondary: Color(0xff7a2e70),
-    secondaryVariant: Color(0xff9b3a8f),
-    onSecondary: Colors.white,
-    error: Color(0xffe63946),
-    onError: Colors.white,
-  ),
+  primarySwatch: const Color(0xff457b9d).toMaterialColor(),
 );
 
 final darkTheme = ThemeData(
@@ -24,43 +17,7 @@ final darkTheme = ThemeData(
   textTheme: GoogleFonts.openSansTextTheme(
     ThemeData.dark().textTheme,
   ),
-  // colorScheme: const ColorScheme.dark(
-  //   primary: Color(0xff457b9d),
-  //   primaryVariant: Color(0xff1d3557),
-  //   onPrimary: Colors.white,
-  //   secondary: Color(0xff7a2e70),
-  //   secondaryVariant: Color(0xff9b3a8f),
-  //   onSecondary: Colors.white,
-  //   error: Color(0xffe63946),
-  //   onError: Colors.white,
-  // ),
-  primaryColorDark: Colors.blue,
-  primaryColorLight: Colors.green,
-  primaryColor: Colors.yellow,
-  accentColor: Colors.pink,
-  hintColor: Colors.blueAccent,
-  inputDecorationTheme: const InputDecorationTheme(
-    border: UnderlineInputBorder(
-      borderSide: BorderSide(color: Colors.green),
-    ),
-    enabledBorder: UnderlineInputBorder(
-      borderSide: BorderSide(color: Colors.yellow),
-    ),
-    errorBorder: UnderlineInputBorder(
-      borderSide: BorderSide(color: Colors.blue),
-    ),
-    focusedBorder: UnderlineInputBorder(
-      borderSide: BorderSide(color: Colors.orange),
-    ),
-    disabledBorder: UnderlineInputBorder(
-      borderSide: BorderSide(color: Colors.black54),
-    ),
-    focusedErrorBorder: UnderlineInputBorder(
-      borderSide: BorderSide(color: Colors.pink),
-    ),
-    labelStyle: TextStyle(color: Colors.tealAccent),
-    floatingLabelBehavior: FloatingLabelBehavior.always,
-  ),
+  primarySwatch: const Color(0xff457b9d).toMaterialColor(),
 );
 
 extension CustomColorScheme on ColorScheme {
@@ -69,5 +26,3 @@ extension CustomColorScheme on ColorScheme {
   Color get biThrowColor => const Color(0xff800080);
   Color get instantBiThrowColor => const Color(0xff008000);
 }
-
-// e63946,f1faee,a8dadc,457b9d,1d3557
