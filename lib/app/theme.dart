00116d1f1eaf42f9d46b24ad59e927f785c9cb4f
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2021-07-18 22:33:32 +0200

    started new color scheme from throw colors

--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -2,51 +2,62 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../ui_core/ui_core.dart';
-
 final lightTheme = ThemeData.from(
   textTheme: GoogleFonts.openSansTextTheme(
     ThemeData.light().textTheme,
   ),
   colorScheme: const ColorScheme.light(
-    primary: Color(0xff457b9d),
-    primaryVariant: Color(0xff1d3557),
+    primary: Color(0xff447212),
+    primaryVariant: Color(0xff26410a),
     onPrimary: Colors.white,
-    secondary: Color(0xffe63946),
-    secondaryVariant: Color(0xffa32831),
+    secondary: Color(0xff661872),
+    secondaryVariant: Color(0xff3b0e42),
     onSecondary: Colors.white,
     surface: Colors.white,
     onSurface: Colors.black,
-    background: Color(0xffe5eef4),
+    background: Color(0xfff8fff1),
     onBackground: Colors.black,
+    error: Color(0xffa31a24),
+    onError: Colors.white,
   ),
 );
 
-final darkTheme = ThemeData(
-  brightness: Brightness.dark,
+final darkTheme = ThemeData.from(
   textTheme: GoogleFonts.openSansTextTheme(
     ThemeData.dark().textTheme,
   ),
-  primarySwatch: const Color(0xff457b9d).toMaterialColor(),
+  colorScheme: ColorScheme.dark(
+    primary: const Color(0xff447212),
+    primaryVariant: Colors.black,
+    onPrimary: Colors.white,
+    secondary: const Color(0xff661872),
+    secondaryVariant: const Color(0xff3b0e42),
+    onSecondary: Colors.white,
+    surface: Colors.grey[800]!,
+    onSurface: Colors.white,
+    background: Colors.grey[700]!,
+    onBackground: Colors.white,
+    error: const Color(0xffa31a24),
+    onError: Colors.black,
+  ),
 );
 
-// TODO: match theme and throw colors (reuse throw colors for theme)
 extension CustomColorScheme on ColorScheme {
   Color get classicThrowColor => _currentColor(
-        const Color(0xff007aff),
-        const Color(0xff0a84ff),
+        const Color(0xff2133a7),
+        const Color(0xff2133a7),
       );
   Color get equiThrowColor => _currentColor(
-        const Color(0xffff3b30),
-        const Color(0xffff453a),
+        const Color(0xffa31a24),
+        const Color(0xffa31a24),
       );
   Color get biThrowColor => _currentColor(
-        const Color(0xff800080),
-        const Color(0xffbf5af2),
+        const Color(0xff661872),
+        const Color(0xff661872),
       );
   Color get instantBiThrowColor => _currentColor(
-        const Color(0xff008000),
-        const Color(0xff30d158),
+        const Color(0xff447212),
+        const Color(0xff447212),
       );
 
   Color _currentColor(Color lightColor, Color darkColor) =>
