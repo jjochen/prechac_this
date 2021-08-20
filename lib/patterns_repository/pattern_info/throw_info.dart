0b57e19251acc102ab1be4b117d00830bbfa0072
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2021-08-20 03:39:46 +0200

    calculate throw info at given point in time

--- lib/patterns_repository/pattern_info/throw_info.dart
+++ lib/patterns_repository/pattern_info/throw_info.dart
@@ -21,16 +21,17 @@ class ThrowInfo with EquatableMixin {
   final int catchingJuggler;
   final int numberOfThrownObjects;
 
-  static ThrowInfo fromPattern({
+  static ThrowInfo atIndex({
     required Pattern pattern,
     required int juggler,
     required int index,
+    bool normalize = true,
   }) {
     final jugglersOffset = pattern.prechator * juggler.toFraction();
     final point = (index.toFraction() + jugglersOffset).toMixedFraction();
     final pointInTime = point
         .copyWith(
-          whole: point.whole % pattern.period,
+          whole: normalize ? point.whole % pattern.period : point.whole,
         )
         .toFraction();
     final theThrow = pattern.throwAtIndex(index);
@@ -40,11 +41,39 @@ class ThrowInfo with EquatableMixin {
     final numberOfThrownObjects = theThrow.height == 0.toFraction() ? 0 : 1;
 
     return ThrowInfo(
-      pointInTime: pointInTime,
+      pointInTime: pointInTime.reduce(),
       throwingJuggler: juggler,
       throwingSiteswapPosition: index,
       theThrow: theThrow,
-      landingTime: landingTime,
+      landingTime: landingTime.reduce(),
+      catchingJuggler: catchingJuggler,
+      numberOfThrownObjects: numberOfThrownObjects,
+    );
+  }
+
+  static ThrowInfo? atPointInTime({
+    required Pattern pattern,
+    required int juggler,
+    required Fraction pointInTime,
+  }) {
+    final jugglersOffset = pattern.prechator * juggler.toFraction();
+    final indexFraction = (pointInTime - jugglersOffset).reduce();
+    if (!indexFraction.isWhole) {
+      return null;
+    }
+    final index = indexFraction.numerator;
+    final theThrow = pattern.throwAtIndex(index);
+    final landingTime = pointInTime + theThrow.height;
+    final catchingJuggler =
+        (juggler + theThrow.passingIndex) % pattern.numberOfJugglers;
+    final numberOfThrownObjects = theThrow.height == 0.toFraction() ? 0 : 1;
+
+    return ThrowInfo(
+      pointInTime: pointInTime.reduce(),
+      throwingJuggler: juggler,
+      throwingSiteswapPosition: index,
+      theThrow: theThrow,
+      landingTime: landingTime.reduce(),
       catchingJuggler: catchingJuggler,
       numberOfThrownObjects: numberOfThrownObjects,
     );
--- test/patterns_repository/pattern_info/throw_info_test.dart
+++ test/patterns_repository/pattern_info/throw_info_test.dart
@@ -37,9 +37,9 @@ void main() {
         pattern = mockPattern;
       });
 
-      test('juggler 0 - index 0', () {
+      test('juggler 0 - at index 0', () {
         expect(
-          ThrowInfo.fromPattern(pattern: pattern, juggler: 0, index: 0),
+          ThrowInfo.atIndex(pattern: pattern, juggler: 0, index: 0),
           ThrowInfo(
             pointInTime: 0.toFraction(),
             throwingJuggler: 0,
@@ -52,16 +52,50 @@ void main() {
         );
       });
 
-      test('juggler 1 - index 0', () {
+      test('juggler 1 - at index 0', () {
         expect(
-          ThrowInfo.fromPattern(pattern: pattern, juggler: 1, index: 0),
+          ThrowInfo.atIndex(pattern: pattern, juggler: 1, index: 0),
           ThrowInfo(
-            pointInTime: 0.toFraction(),
+            pointInTime: 2.toFraction(),
+            throwingJuggler: 1,
+            throwingSiteswapPosition: 0,
+            theThrow: Throw.self(height: 4),
+            landingTime: 6.toFraction(),
+            catchingJuggler: 1,
+            numberOfThrownObjects: 1,
+          ),
+        );
+      });
+
+      test('juggler 1 - at index 3', () {
+        expect(
+          ThrowInfo.atIndex(pattern: pattern, juggler: 1, index: 3),
+          ThrowInfo(
+            pointInTime: 1.toFraction(),
             throwingJuggler: 1,
             throwingSiteswapPosition: 3,
-            theThrow: Throw.self(height: 1),
-            landingTime: 1.toFraction(),
-            catchingJuggler: 1,
+            theThrow: Throw.pass(height: 1),
+            landingTime: 2.toFraction(),
+            catchingJuggler: 0,
+            numberOfThrownObjects: 1,
+          ),
+        );
+      });
+
+      test('juggler 1 - at point in time 5', () {
+        expect(
+          ThrowInfo.atPointInTime(
+            pattern: pattern,
+            juggler: 1,
+            pointInTime: 5.toFraction(),
+          ),
+          ThrowInfo(
+            pointInTime: 5.toFraction(),
+            throwingJuggler: 1,
+            throwingSiteswapPosition: 3,
+            theThrow: Throw.pass(height: 1),
+            landingTime: 6.toFraction(),
+            catchingJuggler: 0,
             numberOfThrownObjects: 1,
           ),
         );
